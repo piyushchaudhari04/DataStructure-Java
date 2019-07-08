86c8f25bb750ea8b59a71650179cc4b81d87bfae
piyush.chaudhari <piyush.kailash.chaudhari@gmail.com>
2019-07-08 19:49:06 +0530

    basic linkedlist algorithms

--- linkedlist/src/main/java/LinkedList.java
+++ linkedlist/src/main/java/LinkedList.java
@@ -27,6 +27,61 @@ public class LinkedList<T> {
     }
 
     /**
+     * The method reverses the linked list
+     */
+    public void reverseList(){
+        ListNode<T> present = head, back=null;
+        while(present!=null){
+            ListNode<T> temp = present.getNext();
+            present.setNext(back);
+            back = present;
+            present = temp;
+        }
+        head = back;
+    }
+
+    /**
+     * The method removes the duplicate elements and keeps only one copy of an element in a sorted list.
+     */
+    public void removeDuplicateSortedList(){
+        ListNode<T> present = head, back =null;
+        if (head ==null){
+            return;
+        }
+        while(present!=null){
+
+            if(back!=null && back.getData().equals(present.getData())){
+                back.setNext(present.getNext());
+                present = present.getNext();
+            }else{
+                back = present;
+                present = present.getNext();
+            }
+        }
+    }
+
+    public void remove(T data){
+
+        if(head == null){
+            return;
+        }
+        ListNode<T> present = head, back = null;
+        while(present!=null){
+            if(present.getData().equals(data)){
+                if(back!=null){
+                    back.setNext(present.getNext());
+                }
+                else{
+                    head = head.getNext();
+                }
+                break;
+            }
+            back=present;
+            present = present.getNext();
+        }
+    }
+
+    /**
      * The method returns the data in the list.
      * @return
      */
--- linkedlist/src/test/java/LinkedListTest.java
+++ linkedlist/src/test/java/LinkedListTest.java
@@ -6,6 +6,18 @@ import java.util.List;
 
 public class LinkedListTest {
 
+   public LinkedList<Integer> setup(){
+
+       LinkedList<Integer> list = new LinkedList<Integer>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       list.add(4);
+       list.add(5);
+       list.add(6);
+       return list;
+   }
+
     @Test
     public void shouldAddElementInTheListCorrectly(){
        LinkedList<Integer> list = new LinkedList<Integer>();
@@ -20,5 +32,53 @@ public class LinkedListTest {
         List<Integer> expected = Arrays.asList(1,2,3,4,5,6);
 
         Assert.assertEquals(expected,list.getData());
+    }
+
+    @Test
+    public void shouldReverseTheList(){
+
+       //Arrange
+        LinkedList<Integer> list = setup();
+        List<Integer> expected =Arrays.asList(6,5,4,3,2,1);
+        //Act
+        list.reverseList();
+
+        //Assert
+        Assert.assertEquals(expected,list.getData());
+    }
+
+    @Test
+    public void shouldRemoveDuplicatesFromList(){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(6);
+        list.add(6);
+        List<Integer> expected = Arrays.asList(1,2,3,4,5,6);
+        list.removeDuplicateSortedList();
+        Assert.assertEquals(expected,list.getData());
+    }
+
+    @Test
+    public void shouldRemoveTheMentionedElement(){
+
+       LinkedList<Integer> list = setup();
+       List<Integer> expected = Arrays.asList(1,2,3,5,6);
+
+       list.remove(4);
+
+       Assert.assertEquals(expected,list.getData());
+
+       list.remove(1);
+       expected = Arrays.asList(2,3,5,6);
+       Assert.assertEquals(expected,list.getData());
     }
 }
